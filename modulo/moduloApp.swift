c3c06a708f72fc6690caa76f7b01188b24fc87fc
matthewauwasd <matthewauwasd@gmail.com>
2023-07-05 13:33:15 -0700

    Initial Commit

---
+++ modulo/moduloApp.swift
@@ -0,0 +1,17 @@
+//
+//  moduloApp.swift
+//  modulo
+//
+//  Created by Matthew Au on 2023-07-05.
+//
+
+import SwiftUI
+
+@main
+struct moduloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
