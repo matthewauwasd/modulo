abfd69f446b173aba81b9a989599ede41a471621
matthewauwasd <matthewauwasd@gmail.com>
2023-08-22 20:10:27 -0700

    UI update, new view for displaying final calculation and going back to start (WIP)
    Functionality to be updated in separate branch.

---
+++ modulo/FinalResultView.swift
@@ -0,0 +1,20 @@
+//
+//  FinalResultView.swift
+//  modulo
+//
+//  Created by Matthew Au on 2023-08-22.
+//
+
+import SwiftUI
+
+struct FinalResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinalResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalResultView()
+    }
+}
--- modulo/SimpleCalcTipChoiceView.swift
+++ modulo/SimpleCalcTipChoiceView.swift
@@ -10,17 +10,47 @@ import SwiftUI
 struct SimpleCalcTipChoiceView: View {
 
     @ObservedObject var viewModel: RatingViewModel
+    @State private var mealCostInput = ""
     
     var body: some View {
         
+        let ratingCorrelatedAmt = viewModel.selectedRating ?? 0
+        
         ZStack {
             
-            let bingBong = viewModel.selectedRating ?? 0
+            Color(red: 0.0, green: 0.0, blue: 1.0, opacity: 1.0)
+                .ignoresSafeArea()
             
-            Text(String(bingBong))
+            VStack {
+                
+                Spacer()
+                
+                Text("What is the total cost of your meal?")
+                    .font(.custom("DMSans-Bold", size: 30))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 150.0)
+                
+                Spacer()
+                Spacer()
+                
+                TextField("Enter amount", text: $mealCostInput )
+                    
+                    .keyboardType(.decimalPad)
+                    .frame(width: 300.0, height: 100.0)
+                    .font(.custom("DMSans-Bold", size: 30))
+                    .foregroundColor(.black)
+                    .background(.white)
+                    .multilineTextAlignment(.center)
+                    .cornerRadius(20)
+                
+                Spacer()
+                
+                Spacer()
+                
+            }
                         
-            //Color(red: 0.0, green: 0.0, blue: 1.0, opacity: 1.0)
-                //.ignoresSafeArea()
+            
             
         }
         .navigationBarHidden(true)
